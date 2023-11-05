332ee7b0280a9c812ff73ff9dbe0d5c571dd0d7a
Ujang Sparrows <116891033+Zahsey@users.noreply.github.com>
2023-11-06 00:25:16 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css';
+import Select from 'react-select'
+import { useEffect, useState } from 'react';
+
+
+function App() {
+  const [datas, setDatas] = useState([])
+  const [userSelect, setUserSelect] = useState("")
+  const [isShow, setIsShow] = useState(false)
+
+  const getBerries = async () => {
+    const berries = await fetch("https://pokeapi.co/api/v2/berry/")
+    const value = await berries.json()
+    let result = value.results.map(data => {
+      console.log(data)
+      return {
+        label: data.name,
+        value: data.name
+      }
+    })
+    setDatas(result.sort((a,b) => a.label.localeCompare(b.label)))
+  }
+
+  useEffect (() => {
+    getBerries()
+  }, [])
+
+  const handleSubmit = () => {
+    setIsShow(state => !state)
+  }
+
+  const handleChange = (value) => {
+    setUserSelect(value)
+  }
+
+  return (
+    <div className="App">
+      <h1>{isShow ? userSelect :""}</h1>
+      <button onClick={() => handleSubmit()} disabled={!userSelect}>{isShow ? "hide button" : "Show Values"}</button>
+      <Select options={datas} onChange={(e) => handleChange(e.value)} ></Select>
+    </div>
+  );
+}
+
+export default App;
